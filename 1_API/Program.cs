fb092a0aa17ae9891449c7a52e0a39d74e7eae82
Phạm Kiên <taikhoancuaban203@gmail.com>
2023-06-10 20:46:56 +0700

    z

---
+++ 1_API/Program.cs
@@ -0,0 +1,33 @@
+using Data.DbContexts;
+using Data.IRepositories;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(typeof(IAllRepositories<>), typeof(AllRepositories<>));
+builder.Services.AddDbContext<CuaHangDbContext>(c => c.UseSqlServer(@"Server=NUGETRENKI\SQLEXPRESS;Database=NET105;Trusted_Connection=True;"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
